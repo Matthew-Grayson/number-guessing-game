c41070926b7d71383d4c6f402bb374c6a902bc6c
Matthew Grayson <matthew.grayson@associates.cisa.dhs.gov>
2025-11-08 09:31:43 -0600

    feat: implement guessing game logic
    * Update main.rs to generate a random secret number, parse user input, and compare guess using Ordering
    * Add rand dependency in Cargo.toml to support random number generation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::cmp::Ordering;
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
     println!("Please input your guess.");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     let mut guess = String::new();
 
@@ -11,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
